2cfe6c373f8cda682ffcf5d3d1f329dfa5f7de16
Tamires Braga <tamirescamposbraga@gmail.com>
2022-10-03 14:37:44 -0300

    teste

--- routes/garden.routes.js
+++ routes/garden.routes.js
@@ -27,3 +27,72 @@ router.post("/create/:idgarden/:idAuthor", async (req, res) => {
       return res.status(400).json(error);
     }
   });
+
+
+  router.get("/all-garden", attachCurrentUser, async (req, res) => {
+    try {
+      const loggedInUser = req.currentUser;
+      const { idGarden } = req.params;
+  
+ //PRECISA TERMINAR AQUI, ACHO QUE TA FALTANDO COISA
+
+      return res.status(200).json(garden);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error);
+    }
+  });
+
+
+
+  router.put("/idGarden/edit", isAuth, attachCurrentUser, async (req, res) => {
+    try {
+      const loggedInUser = req.currentUser;
+      const { idGarden } = req.params;
+  
+      const editedGarden = await GardenModel.findByIdAndUpdate(
+        idGarden,
+        {
+          ...req.body,
+        },
+        { new: true, runValidators: true }
+      );
+  
+      return res.status(200).json(editedGarden);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error);
+    }
+  });
+  
+
+ // router.delete("/delete/:idgarden")
+
+ router.delete(
+  "/delete/:idgarden",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.currentUser;
+      const { idGarden } = req.params;
+
+      const deletedPost = await GardenModel.findByIdAndDelete(idGarden);
+
+      await GardenModel.findByIdAndUpdate(deletedGarden.author, {
+        $pull: { garden: idGarden},
+      });
+
+      // deleto todos os comentários desse post.
+      await CommentModel.deleteMany({ comments: idComment });
+
+      return res
+        .status(200)
+        .json("Jardim deleteado. Usuário atualizado. Comentários deletados");
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error);
+    }
+  }
+);
+
