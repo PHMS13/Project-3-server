0734f42623a60c924f217068c529cf6f7547c322
PHMS13 <phms.13@hotmail.com>
2022-10-04 16:06:01 -0300

    upar imagens incluído

--- index.js
+++ index.js
@@ -5,29 +5,30 @@ dotenv.config();
 
 const app = express();
 
-import dbConnect  from "./config/db.config.js";
+import dbConnect from "./config/db.config.js";
 dbConnect();
 
-app.use(cors({origin: process.env.REACT_APP_URI}));
+app.use(cors({ origin: process.env.REACT_APP_URI }));
 app.use(express.json());
 
-import UsersRoute from "./routes/users.routes.js"
-app.use("/users", UsersRoute)
+import UsersRoute from "./routes/users.routes.js";
+app.use("/users", UsersRoute);
 
-import CommentsRoute from "./routes/comments.routes.js"
-app.use("/comments", CommentsRoute)
+import CommentsRoute from "./routes/comments.routes.js";
+app.use("/comments", CommentsRoute);
 
 /* import PostRoute from "./routes/posts.routes.js"
 app.use("/posts", PostsRoute) */
 
-import GardenRoute from "./routes/garden.routes.js"
-app.use("/garden", GardenRoute)
+import GardenRoute from "./routes/garden.routes.js";
+app.use("/garden", GardenRoute);
 
-import PlantRoute from "./routes/plant.routes.js"
-app.use("/plant", PlantRoute)
+import PlantRoute from "./routes/plant.routes.js";
+app.use("/plant", PlantRoute);
 
+import UploadImgRoute from "./routes/uploadimg.route.js";
+app.use("/", UploadImgRoute);
 
 app.listen(Number(process.env.PORT), () => {
   console.log("Server up and running on port", process.env.PORT);
 });
-
---
+++ routes/uploadimg.route.js
@@ -0,0 +1,17 @@
+import express from "express"
+const router = express.Router();
+
+//middleware
+import uploadimg from "../config/cloudnary.config"
+
+//localhost:4000/upload-image
+router.post("/upload-image", uploadimg.single("picture"), (req, res) => {
+  //se não vier um file na requisição
+  if (!req.file) {
+    return res.status(400).json({ message: "Upload failed." });
+  }
+
+  return res.status(200).json({ url: req.file.path });
+});
+
+export default router
