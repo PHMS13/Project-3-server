e547013903fe84c67924fd2705ce8e16ecae081e
PHMS13 <phms.13@hotmail.com>
2022-10-06 19:12:23 -0300

    cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -8,7 +8,7 @@ const app = express();
 import dbConnect from "./config/db.config.js";
 dbConnect();
 
-app.use(cors({ origin: process.env.REACT_APP_URI }));
+//app.use(cors({ origin: process.env.REACT_APP_URI }));
 app.use(express.json());
 
 import UsersRoute from "./routes/users.routes.js";
